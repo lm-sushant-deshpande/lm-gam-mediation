42d03c955c432181d8be75bc673cf332c150ef3f
Lakhan.Balmiki@lemmatechnologies.com <Lakhan.Balmiki@lemmatechnologies.com>
2024-08-26 11:54:30 +0530

    GAM mediation adapter changes for BannerAd

--- GAM_SampleApp/gam_mediation_adapter/src/main/java/com/lemma/gam_mediation_adapter/LMBannerAd.java
+++ GAM_SampleApp/gam_mediation_adapter/src/main/java/com/lemma/gam_mediation_adapter/LMBannerAd.java
@@ -10,22 +10,53 @@ import com.google.android.gms.ads.mediation.MediationBannerAdCallback;
 import com.google.android.gms.ads.mediation.MediationBannerAdConfiguration;
 
 import lemma.lemmavideosdk.banner.LMBannerView;
+import lemma.lemmavideosdk.banner.LMBannerView.BannerViewListener;
+import lemma.lemmavideosdk.banner.LMBannerView.LMAdSize;
 
 public class LMBannerAd implements MediationBannerAd {
-    private LMBannerView banner;
 
-    public LMBannerAd(@NonNull MediationBannerAdConfiguration mediationBannerAdConfiguration, @NonNull MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> callback) {
+    private final LMBannerView banner;
+    private final MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> adLoadCallback;
+    private MediationBannerAdCallback adCallback;
 
+    public LMBannerAd(@NonNull MediationBannerAdConfiguration mediationBannerAdConfiguration,
+                      @NonNull MediationAdLoadCallback<MediationBannerAd, MediationBannerAdCallback> callback) {
+        this.adLoadCallback = callback;
+
+        // Extract necessary parameters from mediationBannerAdConfiguration
+        String pubId = mediationBannerAdConfiguration.getServerParameters().getString("pubId");
+        String adUnitId = mediationBannerAdConfiguration.getServerParameters().getString("adUnitId");
+        String adServerUrl = mediationBannerAdConfiguration.getServerParameters().getString("adServerUrl");
+
+        // Get the ad size from the configuration
+        int width = mediationBannerAdConfiguration.getAdSize().getWidth();
+        int height = mediationBannerAdConfiguration.getAdSize().getHeight();
+        LMAdSize adSize = new LMAdSize(width, height);
+
+        // Initialize LMBannerView
+        this.banner = new LMBannerView(mediationBannerAdConfiguration.getContext(), pubId, adUnitId, adSize, adServerUrl);
+        this.banner.setBannerViewListener(new BannerViewListener() {
+            @Override
+            public void onAdReceived() {
+                adCallback = adLoadCallback.onSuccess(LMBannerAd.this);
+                adCallback.reportAdImpression();
+            }
+
+            @Override
+            public void onAdError(Error error) {
+                adLoadCallback.onFailure(error.getMessage());
+            }
+        });
     }
 
     public void loadAd() {
-    //        LMBannerView banner = new LMBannerView();
-    //        banner.loadAd();
+        // Load the ad into the banner view
+        banner.loadAd();
     }
 
     @NonNull
     @Override
     public View getView() {
-        return null;
+        return banner;
     }
 }
